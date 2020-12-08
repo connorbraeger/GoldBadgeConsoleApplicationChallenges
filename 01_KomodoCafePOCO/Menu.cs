20af06e3a92246c3fa5b3bd8efbe93c84d1814ce
Connor Braeger <cjbraeger@gmail.com>
2020-12-07 19:11:46 -0500

    first commit, built out menu class

---
+++ 01_KomodoCafePOCO/Menu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_KomodoCafePOCO
+{
+    public class Menu
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string  Description{ get; set; }
+        public List<string> IngredientList { get; set; }
+        public decimal Price { get; set; }
+        public Menu()
+        {
+            Number = 0;
+            Name = "";
+            Description = "";
+            IngredientList = new List<string>();
+            Price = 0;
+        }
+        public Menu(int num, string name, string description, List<string> ingredientList, decimal price)
+        {
+            Number = num;
+            Name = name;
+            Description = description;
+            IngredientList = ingredientList;
+            Price = price;
+        }
+        public Menu (Menu menu)// Copy Constructor
+        {
+            Number = menu.Number;
+            Name = menu.Name;
+            Description = menu.Description;
+            IngredientList = menu.IngredientList;
+            Price = menu.Price;
+        }
+    }
+}
